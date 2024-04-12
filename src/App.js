d7b6741cef7bbc6308738378df589e2677d007c7
prakashup <prakashupadhyay770@gmail.com>
2024-04-12 16:05:48 +0530

    Inital commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className=" text-gray-600 font-extrabold">Hello</header>
+      <header className=" text-gray-600 font-extrabold">Welcome to NetFlix GPT</header>
     </div>
   );
 }
